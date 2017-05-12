a77bc0d310fca1bb101ab4b8e60c6824b3ee9433
Daniel Paul Carbone <dcarbone@ena.com>
2017-05-12 08:55:46 -0500

    Adding context to messages

--- message.go
+++ message.go
@@ -1,66 +1,73 @@
 package turnpike
 
+import (
+	"context"
+	"fmt"
+	"reflect"
+)
+
 // Message is a generic container for a WAMP message.
 type Message interface {
 	MessageType() MessageType
+	Context() context.Context
 }
 
 type MessageType int
 
-func (mt MessageType) New() Message {
+func (mt MessageType) New(ctx context.Context) Message {
 	switch mt {
 	case HELLO:
-		return new(Hello)
+		return &Hello{ctx: ctx}
 	case WELCOME:
-		return new(Welcome)
+		return &Welcome{ctx: ctx}
 	case ABORT:
-		return new(Abort)
+		return &Abort{ctx: ctx}
 	case CHALLENGE:
-		return new(Challenge)
+		return &Challenge{ctx: ctx}
 	case AUTHENTICATE:
-		return new(Authenticate)
+		return &Authenticate{ctx: ctx}
 	case GOODBYE:
-		return new(Goodbye)
+		return &Goodbye{ctx: ctx}
 	case ERROR:
-		return new(Error)
+		return &Error{ctx: ctx}
 
 	case PUBLISH:
-		return new(Publish)
+		return &Publish{ctx: ctx}
 	case PUBLISHED:
-		return new(Published)
+		return &Published{ctx: ctx}
 
 	case SUBSCRIBE:
-		return new(Subscribe)
+		return &Subscribe{ctx: ctx}
 	case SUBSCRIBED:
-		return new(Subscribed)
+		return &Subscribed{ctx: ctx}
 	case UNSUBSCRIBE:
-		return new(Unsubscribe)
+		return &Unsubscribe{ctx: ctx}
 	case UNSUBSCRIBED:
-		return new(Unsubscribed)
+		return &Unsubscribed{ctx: ctx}
 	case EVENT:
-		return new(Event)
+		return &Event{ctx: ctx}
 
 	case CALL:
-		return new(Call)
+		return &Call{ctx: ctx}
 	case CANCEL:
-		return new(Cancel)
+		return &Cancel{ctx: ctx}
 	case RESULT:
-		return new(Result)
+		return &Result{ctx: ctx}
 
 	case REGISTER:
-		return new(Register)
+		return &Register{ctx: ctx}
 	case REGISTERED:
-		return new(Registered)
+		return &Registered{ctx: ctx}
 	case UNREGISTER:
-		return new(Unregister)
+		return &Unregister{ctx: ctx}
 	case UNREGISTERED:
-		return new(Unregistered)
+		return &Unregistered{ctx: ctx}
 	case INVOCATION:
-		return new(Invocation)
+		return &Invocation{ctx: ctx}
 	case INTERRUPT:
-		return new(Interrupt)
+		return &Interrupt{ctx: ctx}
 	case YIELD:
-		return new(Yield)
+		return &Yield{ctx: ctx}
 	default:
 		// TODO: allow custom message types?
 		return nil
@@ -122,8 +129,7 @@ func (mt MessageType) String() string {
 	case YIELD:
 		return "YIELD"
 	default:
-		// TODO: allow custom message types?
-		panic("Invalid message type")
+		return "CUSTOM"
 	}
 }
 
@@ -156,6 +162,8 @@ const (
 	INVOCATION   MessageType = 68 //	Tx 	Rx
 	INTERRUPT    MessageType = 69 //	Tx 	Rx
 	YIELD        MessageType = 70 //	Rx 	Tx
+
+	CUSTOM_MESSAGE MessageType = 80
 )
 
 // URIs are dot-separated identifiers, where each component *should* only contain letters, numbers or underscores.
@@ -168,6 +176,7 @@ type ID uint64
 
 // [HELLO, Realm|uri, Details|dict]
 type Hello struct {
+	ctx     context.Context
 	Realm   URI
 	Details map[string]interface{}
 }
@@ -176,8 +185,13 @@ func (msg *Hello) MessageType() MessageType {
 	return HELLO
 }
 
+func (msg *Hello) Context() context.Context {
+	return msg.ctx
+}
+
 // [WELCOME, Session|id, Details|dict]
 type Welcome struct {
+	ctx     context.Context
 	Id      ID
 	Details map[string]interface{}
 }
@@ -186,8 +200,13 @@ func (msg *Welcome) MessageType() MessageType {
 	return WELCOME
 }
 
+func (msg *Welcome) Context() context.Context {
+	return msg.ctx
+}
+
 // [ABORT, Details|dict, Reason|uri]
 type Abort struct {
+	ctx     context.Context
 	Details map[string]interface{}
 	Reason  URI
 }
@@ -196,8 +215,13 @@ func (msg *Abort) MessageType() MessageType {
 	return ABORT
 }
 
+func (msg *Abort) Context() context.Context {
+	return msg.ctx
+}
+
 // [CHALLENGE, AuthMethod|string, Extra|dict]
 type Challenge struct {
+	ctx        context.Context
 	AuthMethod string
 	Extra      map[string]interface{}
 }
@@ -206,8 +230,13 @@ func (msg *Challenge) MessageType() MessageType {
 	return CHALLENGE
 }
 
+func (msg *Challenge) Context() context.Context {
+	return msg.ctx
+}
+
 // [AUTHENTICATE, Signature|string, Extra|dict]
 type Authenticate struct {
+	ctx       context.Context
 	Signature string
 	Extra     map[string]interface{}
 }
@@ -216,8 +245,13 @@ func (msg *Authenticate) MessageType() MessageType {
 	return AUTHENTICATE
 }
 
+func (msg *Authenticate) Context() context.Context {
+	return msg.ctx
+}
+
 // [GOODBYE, Details|dict, Reason|uri]
 type Goodbye struct {
+	ctx     context.Context
 	Details map[string]interface{}
 	Reason  URI
 }
@@ -226,10 +260,15 @@ func (msg *Goodbye) MessageType() MessageType {
 	return GOODBYE
 }
 
+func (msg *Goodbye) Context() context.Context {
+	return msg.ctx
+}
+
 // [ERROR, REQUEST.Type|int, REQUEST.Request|id, Details|dict, Error|uri]
 // [ERROR, REQUEST.Type|int, REQUEST.Request|id, Details|dict, Error|uri, Arguments|list]
 // [ERROR, REQUEST.Type|int, REQUEST.Request|id, Details|dict, Error|uri, Arguments|list, ArgumentsKw|dict]
 type Error struct {
+	ctx         context.Context
 	Type        MessageType
 	Request     ID
 	Details     map[string]interface{}
@@ -242,10 +281,15 @@ func (msg *Error) MessageType() MessageType {
 	return ERROR
 }
 
+func (msg *Error) Context() context.Context {
+	return msg.ctx
+}
+
 // [PUBLISH, Request|id, Options|dict, Topic|uri]
 // [PUBLISH, Request|id, Options|dict, Topic|uri, Arguments|list]
 // [PUBLISH, Request|id, Options|dict, Topic|uri, Arguments|list, ArgumentsKw|dict]
 type Publish struct {
+	ctx         context.Context
 	Request     ID
 	Options     map[string]interface{}
 	Topic       URI
@@ -257,8 +301,13 @@ func (msg *Publish) MessageType() MessageType {
 	return PUBLISH
 }
 
+func (msg *Publish) Context() context.Context {
+	return msg.ctx
+}
+
 // [PUBLISHED, PUBLISH.Request|id, Publication|id]
 type Published struct {
+	ctx         context.Context
 	Request     ID
 	Publication ID
 }
@@ -267,8 +316,13 @@ func (msg *Published) MessageType() MessageType {
 	return PUBLISHED
 }
 
+func (msg *Published) Context() context.Context {
+	return msg.ctx
+}
+
 // [SUBSCRIBE, Request|id, Options|dict, Topic|uri]
 type Subscribe struct {
+	ctx     context.Context
 	Request ID
 	Options map[string]interface{}
 	Topic   URI
@@ -278,8 +332,13 @@ func (msg *Subscribe) MessageType() MessageType {
 	return SUBSCRIBE
 }
 
+func (msg *Subscribe) Context() context.Context {
+	return msg.ctx
+}
+
 // [SUBSCRIBED, SUBSCRIBE.Request|id, Subscription|id]
 type Subscribed struct {
+	ctx          context.Context
 	Request      ID
 	Subscription ID
 }
@@ -288,8 +347,13 @@ func (msg *Subscribed) MessageType() MessageType {
 	return SUBSCRIBED
 }
 
+func (msg *Subscribed) Context() context.Context {
+	return msg.ctx
+}
+
 // [UNSUBSCRIBE, Request|id, SUBSCRIBED.Subscription|id]
 type Unsubscribe struct {
+	ctx          context.Context
 	Request      ID
 	Subscription ID
 }
@@ -298,8 +362,13 @@ func (msg *Unsubscribe) MessageType() MessageType {
 	return UNSUBSCRIBE
 }
 
+func (msg *Unsubscribe) Context() context.Context {
+	return msg.ctx
+}
+
 // [UNSUBSCRIBED, UNSUBSCRIBE.Request|id]
 type Unsubscribed struct {
+	ctx     context.Context
 	Request ID
 }
 
@@ -307,11 +376,16 @@ func (msg *Unsubscribed) MessageType() MessageType {
 	return UNSUBSCRIBED
 }
 
+func (msg *Unsubscribed) Context() context.Context {
+	return msg.ctx
+}
+
 // [EVENT, SUBSCRIBED.Subscription|id, PUBLISHED.Publication|id, Details|dict]
 // [EVENT, SUBSCRIBED.Subscription|id, PUBLISHED.Publication|id, Details|dict, PUBLISH.Arguments|list]
 // [EVENT, SUBSCRIBED.Subscription|id, PUBLISHED.Publication|id, Details|dict, PUBLISH.Arguments|list,
 //     PUBLISH.ArgumentsKw|dict]
 type Event struct {
+	ctx          context.Context
 	Subscription ID
 	Publication  ID
 	Details      map[string]interface{}
@@ -321,6 +395,10 @@ type Event struct {
 
 func (msg *Event) MessageType() MessageType {
 	return EVENT
+}
+
+func (msg *Event) Context() context.Context {
+	return msg.ctx
 }
 
 // CallResult represents the result of a CALL.
@@ -334,6 +412,7 @@ type CallResult struct {
 // [CALL, Request|id, Options|dict, Procedure|uri, Arguments|list]
 // [CALL, Request|id, Options|dict, Procedure|uri, Arguments|list, ArgumentsKw|dict]
 type Call struct {
+	ctx         context.Context
 	Request     ID
 	Options     map[string]interface{}
 	Procedure   URI
@@ -345,10 +424,15 @@ func (msg *Call) MessageType() MessageType {
 	return CALL
 }
 
+func (msg *Call) Context() context.Context {
+	return msg.ctx
+}
+
 // [RESULT, CALL.Request|id, Details|dict]
 // [RESULT, CALL.Request|id, Details|dict, YIELD.Arguments|list]
 // [RESULT, CALL.Request|id, Details|dict, YIELD.Arguments|list, YIELD.ArgumentsKw|dict]
 type Result struct {
+	ctx         context.Context
 	Request     ID
 	Details     map[string]interface{}
 	Arguments   []interface{}          `wamp:"omitempty"`
@@ -359,8 +443,13 @@ func (msg *Result) MessageType() MessageType {
 	return RESULT
 }
 
+func (msg *Result) Context() context.Context {
+	return msg.ctx
+}
+
 // [REGISTER, Request|id, Options|dict, Procedure|uri]
 type Register struct {
+	ctx       context.Context
 	Request   ID
 	Options   map[string]interface{}
 	Procedure URI
@@ -370,8 +459,13 @@ func (msg *Register) MessageType() MessageType {
 	return REGISTER
 }
 
+func (msg *Register) Context() context.Context {
+	return msg.ctx
+}
+
 // [REGISTERED, REGISTER.Request|id, Registration|id]
 type Registered struct {
+	ctx          context.Context
 	Request      ID
 	Registration ID
 }
@@ -380,8 +474,13 @@ func (msg *Registered) MessageType() MessageType {
 	return REGISTERED
 }
 
+func (msg *Registered) Context() context.Context {
+	return msg.ctx
+}
+
 // [UNREGISTER, Request|id, REGISTERED.Registration|id]
 type Unregister struct {
+	ctx          context.Context
 	Request      ID
 	Registration ID
 }
@@ -390,8 +489,13 @@ func (msg *Unregister) MessageType() MessageType {
 	return UNREGISTER
 }
 
+func (msg *Unregister) Context() context.Context {
+	return msg.ctx
+}
+
 // [UNREGISTERED, UNREGISTER.Request|id]
 type Unregistered struct {
+	ctx     context.Context
 	Request ID
 }
 
@@ -399,10 +503,15 @@ func (msg *Unregistered) MessageType() MessageType {
 	return UNREGISTERED
 }
 
+func (msg *Unregistered) Context() context.Context {
+	return msg.ctx
+}
+
 // [INVOCATION, Request|id, REGISTERED.Registration|id, Details|dict]
 // [INVOCATION, Request|id, REGISTERED.Registration|id, Details|dict, CALL.Arguments|list]
 // [INVOCATION, Request|id, REGISTERED.Registration|id, Details|dict, CALL.Arguments|list, CALL.ArgumentsKw|dict]
 type Invocation struct {
+	ctx          context.Context
 	Request      ID
 	Registration ID
 	Details      map[string]interface{}
@@ -414,10 +523,15 @@ func (msg *Invocation) MessageType() MessageType {
 	return INVOCATION
 }
 
+func (msg *Invocation) Context() context.Context {
+	return msg.ctx
+}
+
 // [YIELD, INVOCATION.Request|id, Options|dict]
 // [YIELD, INVOCATION.Request|id, Options|dict, Arguments|list]
 // [YIELD, INVOCATION.Request|id, Options|dict, Arguments|list, ArgumentsKw|dict]
 type Yield struct {
+	ctx         context.Context
 	Request     ID
 	Options     map[string]interface{}
 	Arguments   []interface{}          `wamp:"omitempty"`
@@ -428,8 +542,13 @@ func (msg *Yield) MessageType() MessageType {
 	return YIELD
 }
 
+func (msg *Yield) Context() context.Context {
+	return msg.ctx
+}
+
 // [CANCEL, CALL.Request|id, Options|dict]
 type Cancel struct {
+	ctx     context.Context
 	Request ID
 	Options map[string]interface{}
 }
@@ -438,12 +557,43 @@ func (msg *Cancel) MessageType() MessageType {
 	return CANCEL
 }
 
+func (msg *Cancel) Context() context.Context {
+	return msg.ctx
+}
+
 // [INTERRUPT, INVOCATION.Request|id, Options|dict]
 type Interrupt struct {
+	ctx     context.Context
 	Request ID
 	Options map[string]interface{}
 }
 
 func (msg *Interrupt) MessageType() MessageType {
 	return INTERRUPT
+}
+
+func (msg *Interrupt) Context() context.Context {
+	return msg.ctx
+}
+
+type CustomMessage map[string]interface{}
+
+func (msg CustomMessage) MessageType() MessageType {
+	return CUSTOM_MESSAGE
+}
+
+func (msg CustomMessage) Context() context.Context {
+	v, ok := msg["ctx"]
+	if !ok {
+		panic("CustomMessage without context seen")
+	}
+
+	ctx, ok := v.(context.Context)
+	if !ok {
+		panic(fmt.Sprintf(
+			"CustomMessage has \"ctx\" key, but it is not of type \"context.Context\".  Type seen: %s",
+			reflect.TypeOf(ctx)))
+	}
+
+	return ctx
 }
